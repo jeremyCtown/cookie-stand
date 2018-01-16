8c29b34bd47a7fd48d7f723d387db1b49aa578ff
Jeremy Crawford <34947155+JeremyCtown@users.noreply.github.com>
2018-01-15 17:38:11 -0800

    stuck on trying to sum the hourly output of cookies

--- js/sales-app.js
+++ js/sales-app.js
@@ -1,26 +1,32 @@
 'use strict';
 
-var hours = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: '] 
+var hours = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: '];
 
 var pike = {
-  
+  min: 23,
+  max: 65,
+  avg: 6.3,
+  hourlyCookies: [],
+
+  hourlySales: function() {
+    return (Math.floor((Math.random() * (this.max - this.min) + this.min) * this.avg));
+  },
+
+  cookiesByHour: function() 
+
   render: function() {
     var ulEl = document.getElementById('pike');
 
     for (var i = 0; i < hours.length; i++) {
-      var hourlySales = Math.floor(Math.random() * (66 - 23) + 23);
       var liEl = document.createElement('li');
-      liEl.textContent = hours[i] + hourlySales + ' cookies';
+      liEl.textContent = hours[i] + this.hourlySales() + ' cookies';
       ulEl.appendChild(liEl);
     }
-    liEl = document.createElement('li');
-    liEl.textContent = 'Total: cookies';
-    ulEl.appendChild(liEl);
   }
 };
 
 pike.render();
-
+/*
 var seaTac = {
 
 }
@@ -36,3 +42,4 @@ var capHill = {
 var alki = {
 
 }
+*/
