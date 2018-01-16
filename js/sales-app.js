38204864c325c5a0e7287c2224294dafda85f3ec
Jeremy Crawford <34947155+JeremyCtown@users.noreply.github.com>
2018-01-16 10:35:41 -0800

    Code review was very helpful in figuring out how to code the JS, will continue to work on other ideas for solving this problem

--- js/sales-app.js
+++ js/sales-app.js
@@ -1,36 +1,39 @@
 'use strict';
 
-var hours = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: '];
+var hours = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: ', 'Total: '];
 
 var pike = {
-  min: 23,
-  max: 65,
-  avg: 6.3,
-  totalCookies: 'A bunch of ',
+  minCustomers: 23,
+  maxCustomers: 65,
+  avgCookiesPerCustomer: 6.3,
 
-  hourlyCookies: function() {
-    return ((Math.floor((Math.random() * (this.max - this.min) + this.min) * this.avg)));
+  hourlyCookies: function() { //this generates a random number of customers per hour and multiplies it by the average number of cookies
+    return Math.round((Math.floor((Math.random() * (this.maxCustomers - this.minCustomers + 1) + this.minCustomers))) * this.avgCookiesPerCustomer) + ' cookies';
   },
 
-  /*totalCookiesCounter: function() {
-
-    for(var i = 0; i < hours.length; i++) {
-      this.totalCookies += this.hourlyCookies();
-      console.log('There are a total of' + this.totalCookies);
+  cookieSales: function () {
+    var salesByHour = [];
+    var totalCookies = 0;
+    for(var i = 0 ; i < hours.length ; i++) {
+      if(i === (hours.length - 1)) { //this is for last item in array
+        salesByHour[i] = totalCookies + ' cookies';
+      } else { //this runs until we get to last element
+        salesByHour[i] = this.hourlyCookies(); //this adds values to 'salesByHour'
+        totalCookies += parseInt(salesByHour[i]);
+      }
     }
-  },*/
+    return salesByHour;
+  },
 
   render: function() {
     var ulEl = document.getElementById('pike');
-
+    var todaySales = this.cookieSales();
     for (var i = 0; i < hours.length; i++) {
       var liEl = document.createElement('li');
-      liEl.textContent = hours[i] + this.hourlyCookies() + ' cookies';
+      liEl.textContent = hours[i] + todaySales[i];
+      console.log (hours[i] + todaySales[i]);
       ulEl.appendChild(liEl);
     }
-    liEl = document.createElement('li');
-    liEl.textContent = 'Total: ' + this.totalCookies + ' cookies';
-    ulEl.appendChild(liEl);
   }
 };
 
